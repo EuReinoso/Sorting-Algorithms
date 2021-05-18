59952f25ca37236dcb47188eb8f94f8ae72ce682
EuReinoso <lucas.reinoso.br@gmail.com>
2021-05-18 20:15:17 -0300

    suffle bar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from bar import Bar
+from random import shuffle
 
 pygame.init()
 
@@ -11,7 +12,7 @@ window = pygame.display.set_mode(WINDOW_SIZE)
 def bars_init(x, y, width, size):
     bar_list = []
     height = 1
-    height_increment = WINDOW_SIZE[1]/size
+    height_increment = y/size
     for i in range(size):
         rect = pygame.Rect(x, y - height, width, height)
         bar_list.append(Bar(rect))
@@ -23,11 +24,24 @@ def draw_bars(bar_list):
     for i in range(len(bar_list)):
         bar_list[i].draw(window)
 
+def bar_shuffle(bar_list):
+    pos_list = []
+    for i in range(len(bar_list)):
+        pos = bar_list[i].rect.x
+        pos_list.append(pos)
+
+    shuffle(pos_list)
+
+    for i in range(len(pos_list)):
+        bar_list[i].rect.x = pos_list[i]
+
 def main():
 
     bar_quant = 100
     bar_width = WINDOW_SIZE[0]/bar_quant
     bar_list = bars_init(0, WINDOW_SIZE[1], bar_width, bar_quant)
+
+    bar_shuffle(bar_list)
 
     loop = True
     while loop:
