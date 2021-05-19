323f4014420fbd948a8b4c5830efe8c4e7e1979e
EuReinoso <lucas.reinoso.br@gmail.com>
2021-05-19 09:59:32 -0300

    set height bar

--- bar.py
+++ bar.py
@@ -9,3 +9,10 @@ class Bar:
     
     def draw(self, window):
         pygame.draw.rect(window, (200, 200, 200), self.rect)
+
+    def set_height(self, value):
+        window_size_y = self.rect.y + self.rect.height
+        self.rect.height = value
+        self.rect.y =  window_size_y - self.rect.height 
+        self.value = value
+        
