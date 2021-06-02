3b0e51fcad1b28ef64244bcd8d603c765e254b8d
EuReinoso <lucas.reinoso.br@gmail.com>
2021-06-02 20:26:53 -0300

    tim sort

--- main.py
+++ main.py
@@ -393,6 +393,106 @@ def cocktail_sort(values):
   
         start = start+1
 
+MIN_MERGE = 32
+def calc_min_run(n):
+    r = 0
+    while n >= MIN_MERGE:
+        r |= n & 1
+        n >>= 1
+    return n + r
+
+def insert_sort2(values, left, right):
+    for i in range(left + 1, right + 1):
+        j = i
+        while j > left and values[j] < values[j - 1]:
+            values[j], values[j - 1] = values[j - 1], values[j]
+            bar_list[j].set_height(values[j])
+            bar_list[j - 1].set_height(values[j - 1])
+
+            window_updates()
+            pygame.draw.rect(window, (200, 0, 0), bar_list[j].rect)
+            pygame.display.update()
+
+            j -= 1
+
+def merge(values, l, m, r):
+     
+    len1, len2 = m - l + 1, r - m
+    left, right = [], []
+    for i in range(0, len1):
+        left.append(values[l + i])
+    for i in range(0, len2):
+        right.append(values[m + 1 + i])
+ 
+    i, j, k = 0, 0, l
+     
+    while i < len1 and j < len2:
+        if left[i] <= right[j]:
+            values[k] = left[i]
+            bar_list[k].set_height(values[k])
+
+            window_updates()
+            pygame.draw.rect(window, (200, 0, 0), bar_list[k].rect)
+            pygame.display.update()
+
+
+            i += 1
+ 
+        else:
+            values[k] = right[j]
+            bar_list[k].set_height(values[k])
+
+            window_updates()
+            pygame.draw.rect(window, (200, 0, 0), bar_list[k].rect)
+            pygame.display.update()
+
+            j += 1
+ 
+        k += 1
+ 
+    while i < len1:
+        values[k] = left[i]
+        bar_list[k].set_height(values[k])
+
+        window_updates()
+        pygame.draw.rect(window, (200, 0, 0), bar_list[k].rect)
+        pygame.display.update()
+
+        k += 1
+        i += 1
+ 
+    while j < len2:
+        values[k] = right[j]
+        bar_list[k].set_height(values[k])
+
+        window_updates()
+        pygame.draw.rect(window, (200, 0, 0), bar_list[k].rect)
+        pygame.display.update()
+
+        k += 1
+        j += 1
+
+def tim_sort(values):
+    n = len(values)
+    minRun = calc_min_run(n)
+     
+    for start in range(0, n, minRun):
+        end = min(start + minRun - 1, n - 1)
+        insert_sort2(values, start, end)
+ 
+    size = minRun
+    while size < n:
+         
+        for left in range(0, n, 2 * size):
+ 
+            mid = min(n - 1, left + size - 1)
+            right = min((left + 2 * size - 1), (n - 1))
+ 
+            if mid < right:
+                merge(values, left, mid, right)
+ 
+        size = 2 * size
+
 def window_updates():
     window.fill((0, 0, 0))
     draw_bars()
@@ -431,8 +531,8 @@ def main():
 
     algorithms_buttons_pos = [WINDOW_SIZE[0] * 0.05, WINDOW_SIZE[1] * 0.82]
     algorithms_buttons_size = (WINDOW_SIZE[0] * 0.1, WINDOW_SIZE[1] * 0.05)
-    algorithms_buttons = buttons_sequence(11, algorithms_buttons_pos, algorithms_buttons_size, 
-                                        texts= ['Insert', 'Selection', 'Bubble', 'Comb', 'Merge', 'Heap', 'Shell', 'Radix', 'Gnome', 'Counting','Cocktail'],
+    algorithms_buttons = buttons_sequence(12, algorithms_buttons_pos, algorithms_buttons_size, 
+                                        texts= ['Insert', 'Selection', 'Bubble', 'Comb', 'Merge', 'Heap', 'Shell', 'Radix', 'Gnome', 'Counting','Cocktail','Tim'],
                                         space_x= WINDOW_SIZE[0] * 0.003, space_y= WINDOW_SIZE[1] * 0.003, break_point= 4)
     algorithms_buttons[0].selected = True
 
@@ -488,6 +588,9 @@ def main():
                 if algorithm_name == 'Cocktail':
                     values = get_values(bar_list)
                     cocktail_sort(values)
+                if algorithm_name == 'Tim':
+                    values = get_values(bar_list)
+                    tim_sort(values)
                 
             for i in range(len(algorithms_buttons)):
                 if algorithms_buttons[i].click(event, mx, my):
